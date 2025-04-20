cade06d49450c4169eb5e8f71353d43c0908ee68
alexis.vasseur <alexis.vasseur@epita.fr>
2025-04-20 22:16:51 +0200

    update: add comments on vector functions

--- src/vector/vector.h
+++ src/vector/vector.h
@@ -12,12 +12,62 @@ typedef struct vector {
     free_function free_function;
 } vector;
 
+/**
+ * Initializes a vector with a custom free function.
+ *
+ * @param vec Pointer to the vector to initialize.
+ * @param free_function Free function to use for the vector's elements.
+ */
 void vector_init(vector *vec, free_function free_function);
+
+/**
+ * Adds an element to the end of the vector.
+ *
+ * @param vec Pointer to the vector.
+ * @param data Pointer to the data to add.
+ */
 void vector_add(vector *vec, void *data);
-void vector_set(vector *vec, int, void *data);
+
+/**
+ * Replaces the element at a specific index in the vector.
+ *
+ * @param vec Pointer to the vector.
+ * @param index Index of the element to replace.
+ * @param data Pointer to the new data.
+ */
+void vector_set(vector *vec, int index, void *data);
+
+/**
+ * Retrieves the element at a specific index in the vector.
+ *
+ * @param vec Pointer to the vector.
+ * @param index Index of the element to retrieve.
+ * @return Pointer to the element's data.
+ */
 void *vector_get(vector *vec, int index);
+
+/**
+ * Deletes the element at a specific index in the vector.
+ *
+ * @param vec Pointer to the vector.
+ * @param index Index of the element to delete.
+ */
 void vector_delete(vector *vec, int index);
+
+/**
+ * Removes and returns the last element of the vector.
+ *
+ * @param vec Pointer to the vector.
+ * @return Pointer to the data of the removed element.
+ */
 void *vector_pop(vector *vec);
+
+/**
+ * Frees the memory allocated for the vector and its elements.
+ *
+ * @param vec Pointer to the vector to free.
+ */
 void vector_free(vector *vec);
+
 
 #endif /* VECTOR_H */
